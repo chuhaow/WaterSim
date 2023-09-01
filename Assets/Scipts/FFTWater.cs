4ae192611d558c6864aff843f7a663ed91a12122
Chu Hao Wen <chuhaow@gmail.com>
2023-09-01 15:39:32 -0500

    displace by height map

---
+++ Assets/Scipts/FFTWater.cs
@@ -0,0 +1,210 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static System.Runtime.InteropServices.Marshal;
+using UnityEngine.Rendering;
+[RequireComponent(typeof(MeshFilter))]
+public class FFTWater : MonoBehaviour
+{
+    [Serializable]
+    public struct Wave
+    {
+        [Range(0.01f, 5.0f)]
+        [SerializeField] private float amplitude;
+        [Range(0.01f, 5.0f)]
+        [SerializeField] private float frequency;
+        [SerializeField] private Vector2 direction;
+        [SerializeField] private float phase;
+        [SerializeField] private float sharpness;
+
+        public float CalulateSine(float x)
+        {
+            return amplitude * Mathf.Sin(x * frequency + Time.time);
+        }
+    }
+
+    [Serializable]
+    public struct FBMParam
+    {
+        [SerializeField] private float amplitude;
+        [SerializeField] private float frequency;
+        [SerializeField] private float speed;
+        [SerializeField] private float seed;
+        [SerializeField] private float sharpness;
+        [SerializeField] private float ampMult;
+        [SerializeField] private float frequencyMult;
+        [SerializeField] private float speedMult;
+        [SerializeField] private float seedIncrement;
+        [SerializeField] private float sharpnessMult;
+        [SerializeField] private int _FBMCount;
+
+        public float Amplitude { get => amplitude; set => amplitude = value; }
+        public float Frequency { get => frequency; set => frequency = value; }
+        public float Speed { get => speed; set => speed = value; }
+        public float Seed { get => seed; set => seed = value; }
+        public float Sharpness { get => sharpness; set => sharpness = value; }
+        public float AmpMult { get => ampMult; set => ampMult = value; }
+        public float FrequencyMult { get => frequencyMult; set => frequencyMult = value; }
+        public float SpeedMult { get => speedMult; set => speedMult = value; }
+        public float SeedIncrement { get => seedIncrement; set => seedIncrement = value; }
+        public float SharpnessMult { get => sharpnessMult; set => sharpnessMult = value; }
+        public int FBMCount { get => _FBMCount; set => _FBMCount = value; }
+    }
+
+
+
+    [SerializeField] private int lengthX = 10;
+    [SerializeField] private int lengthZ = 10;
+    [SerializeField] private int quadRes = 10;
+    [SerializeField] private Wave[] waves;
+    [SerializeField] private Shader waterShader;
+    [SerializeField] private ComputeShader cs;
+
+    [SerializeField] private Color Ambient;
+    [SerializeField] private Color Diffuse;
+    [SerializeField] private Color Specular;
+    [SerializeField] private float Reflectance;
+
+    [SerializeField] private float Lacunarity;
+    [Range(0.0f, 1.0f)]
+    [SerializeField] private float Gain;
+    private Mesh mesh;
+    private Vector3[] vertices;
+    private Vector3[] normals;
+    private ComputeBuffer waveBuffer;
+
+    private Material waterMat;
+
+    private RenderTexture heightTex, normTex;
+
+    [Header("FBM Param")]
+    [SerializeField] private FBMParam WaveFBM;
+    [SerializeField] private FBMParam NormFBM;
+
+
+    private void OnEnable()
+    {
+        CreatePlane();
+        CreateMaterial();
+        CreateTextures();
+        //CreateBuffer();
+
+    }
+
+    private void CreateTextures()
+    {
+        heightTex = new RenderTexture(512, 512, 0, RenderTextureFormat.RHalf, RenderTextureReadWrite.Linear);
+        heightTex.enableRandomWrite = true;
+        heightTex.Create();
+
+        normTex = new RenderTexture(512, 512, 0, RenderTextureFormat.ARGB64, RenderTextureReadWrite.Linear);
+        normTex.enableRandomWrite = true;
+        normTex.Create();
+
+        cs.SetTexture(0, "_HeightTex", heightTex);
+        cs.SetTexture(0, "_NormalTex", normTex);
+        cs.Dispatch(0, Mathf.CeilToInt(Screen.width / 8.0f), Mathf.CeilToInt(Screen.height / 8.0f), 1);
+
+    }
+
+    private void CreateMaterial()
+    {
+        if (waterShader == null) return;
+        if (waterMat != null) return;
+        waterMat = new Material(waterShader);
+        //waterMat.SetInt("_WavesLength", waves.Length);
+
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+
+        renderer.material = waterMat;
+
+    }
+
+    private void CreateBuffer()
+    {
+        waveBuffer = new ComputeBuffer(waves.Length, SizeOf(typeof(Wave)));
+        waterMat.SetBuffer("_Waves", waveBuffer);
+    }
+
+    //ref: https://catlikecoding.com/unity/tutorials/procedural-grid/
+    private void CreatePlane()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        mesh.name = "procedural Plane";
+
+        int sideVertCountX = lengthX * quadRes;
+        int sideVertCountZ = lengthZ * quadRes;
+
+        vertices = new Vector3[(sideVertCountX + 1) * (sideVertCountZ + 1)];
+        Vector2[] uv = new Vector2[vertices.Length];
+        for (int z = 0, i = 0; z <= sideVertCountZ; z++)
+        {
+            for (int x = 0; x <= sideVertCountX; x++)
+            {
+                vertices[i] = new Vector3(x, 0, z);
+                uv[i] = new Vector2((float)x / sideVertCountX, (float)z / sideVertCountZ);
+                i++;
+            }
+        }
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        int[] triangles = new int[sideVertCountX * sideVertCountZ * 6];
+        for (int tris = 0, vert = 0, z = 0; z < sideVertCountZ; z++, vert++)
+        {
+            for (int x = 0; x < sideVertCountX; x++, tris += 6, vert++)
+            {
+                triangles[tris] = vert;
+                triangles[tris + 3] = triangles[tris + 2] = vert + 1;
+                triangles[tris + 4] = triangles[tris + 1] = vert + sideVertCountX + 1;
+                triangles[tris + 5] = vert + sideVertCountX + 2;
+            }
+        }
+
+        mesh.triangles = triangles;
+    }
+
+    private void Update()
+    {
+
+        cs.SetTexture(0, "_HeightTex", heightTex);
+        cs.SetTexture(0, "_NormalTex", normTex);
+        cs.SetFloat("_FrameTime", Time.time);
+        cs.Dispatch(0, Mathf.CeilToInt(Screen.width / 8.0f), Mathf.CeilToInt(Screen.height / 8.0f), 1);
+        waterMat.SetTexture("_HeightTex", heightTex);
+        waterMat.SetTexture("_NormalTex", normTex);
+
+        //for(int i = 0; i < vertices.Length; i++)
+        //{
+        //    Vector3 vert = transform.TransformPoint(vertices[i]);
+        //    float height = SumWaves(vert.x * vert.z);
+        //    vertices[i].y = height;
+        //}
+        //mesh.vertices = vertices;
+    }
+
+    private float SumWaves(float x)
+    {
+        float result = 0;
+        for (int i = 0; i < waves.Length; i++)
+        {
+            result += waves[i].CalulateSine(x);
+        }
+        return result;
+    }
+
+    private void OnDestroy()
+    {
+        waveBuffer.Dispose();
+    }
+
+    private void OnDrawGizmos()
+    {
+        //if (vertices == null) return;
+        //for(int i = 0; i < vertices.Length; i++)
+        //{
+        //    Gizmos.DrawSphere(vertices[i], 0.1f);
+        //}
+    }
+}
---
+++ Assets/Shader/FFTWater.shader
@@ -0,0 +1,193 @@
+Shader "Unlit/FFTWater"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+            #include "UnityPBSLighting.cginc"
+            #include "AutoLight.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+                float3 normal : NORMAL;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+                float3 normal : NORMAL;
+                float3 worldPos: TEXCOORD1;
+            };
+
+            struct Wave
+            {
+                float amplitude;
+                float frequency;
+                float2 direction;
+                float phase;
+                float sharpness;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            float F0;
+            float _BaseGain;
+            float _BaseLacunarity;
+            StructuredBuffer<Wave> _Waves;
+            int _WavesLength;
+
+            float _WaveAmp, _WaveFreq, _WaveSpeed, _WaveSeed, _WaveSharpness, _WaveAmpMult, _WaveFreqMult, _WaveSharpnessMult, _WaveSpeedMult, _WaveSeedIncrement, _WaveCount;
+            float _NormAmp, _NormFreq, _NormSpeed, _NormSeed, _NormSharpness, _NormAmpMult, _NormFreqMult, _NormSharpnessMult, _NormSpeedMult, _NormSeedIncrement, _NormFBMCount;
+
+            float4 _Ambient, _Diffuse, _Specular;
+            
+            SamplerState linear_repeat_sampler;
+            Texture2D _HeightTex;
+            sampler2D _NormalTex;
+
+            float Sine(float3 vert, Wave w) {
+                float time = _Time.y * w.phase;
+                float2 d = w.direction;
+                float xz = d.x * vert.x + d.y * vert.z;
+                return w.amplitude * sin(w.frequency * xz + time);
+            }
+            //https://catlikecoding.com/unity/tutorials/flow/waves/
+
+            float3 FBM(float3 vert) {
+                float freq = _WaveFreq;
+                float amp = _WaveAmp;
+                float speed = _WaveSpeed;
+                float rnd = _WaveSeed;
+                float ampSum = 0.0f;
+                float sharpness = _WaveSharpness;
+                float3 h = 0.0f;
+                for (int i = 0; i < _WaveCount; i++) {
+                    float2 dir = normalize(float2(cos(rnd), sin(rnd)));
+                    float xz = dir.x * vert.x + dir.y * vert.z;
+                    float3 wave = float3(0.0f, 0.0f, 0.0f);
+                    wave.x = dir.x * sharpness * amp * cos(freq * xz + _Time.y * speed);
+                    wave.z = dir.y * sharpness * amp * cos(freq * xz + _Time.y * speed);
+                    wave.y = amp * sin(freq * xz + _Time.y * speed);
+                    //float wave = amp * sin(freq * xz + _Time.y * speed);
+                    ampSum += amp;
+                    h += wave;
+                    freq *= _WaveFreqMult;
+                    amp *= _WaveAmpMult;
+                    sharpness *= _WaveSharpnessMult;
+                    speed *= _WaveSpeedMult;
+                    rnd += _WaveSeedIncrement;
+                }
+
+                return h/ampSum;
+            }
+
+            float3 FBMNormal(float3 vert) {
+                float freq = _NormFreq;
+                float amp = _NormAmp;
+                float speed = _NormSpeed;
+                float rnd = _NormSeed;
+                float ampSum = 0.0f;
+                float sharpness = _NormSharpness;
+                float3 n = 0.0f;
+                for (int i = 0; i < _NormFBMCount; i++) {
+                    float2 dir = normalize(float2(cos(rnd), sin(rnd)));
+                    float xz = dir.x * vert.x + dir.y * vert.z;
+                    float3 norm = float3(0.0f, 0.0f, 0.0f);
+
+                    float wa = freq * amp;
+                    float s = sin(freq * xz + _Time.y * speed);
+                    float c = cos(freq * xz + _Time.y * speed);
+
+                    norm.x = dir.x * wa * c;
+                    norm.z = dir.y * wa * c;
+                    norm.y = sharpness * wa * s;
+
+                    ampSum += amp;
+                    n += norm;
+                    freq *= _NormFreqMult;
+                    amp *= _NormAmpMult;
+                    sharpness *= _NormSharpnessMult;
+                    speed *= _NormSpeedMult;
+                    rnd += _NormSeedIncrement;
+                }
+
+                return n / ampSum;
+            }
+
+            float3 Normal(float3 vert, Wave w) {
+                float2 d = w.direction;
+                float xz = d.x * vert.x + d.y * vert.z;
+                float time = _Time.y * w.phase;
+                float2 n = w.frequency * w.amplitude * d * cos(xz * w.frequency + time);
+
+                return float3(n, 0.0f);
+            }
+
+            //Fresnel from https://developer.nvidia.com/gpugems/gpugems3/part-iii-rendering/chapter-14-advanced-techniques-realistic-real-time-skin
+            float4 BlinnPhone(float3 p) {
+                float3 lightDir = _WorldSpaceLightPos0;
+                float3 E = normalize(_WorldSpaceCameraPos - p);
+                float3 H = normalize(lightDir + E);
+                float3 N = 0.0f;
+
+                N = FBMNormal(p);
+                N = normalize(UnityObjectToWorldNormal(normalize(float3(-N.x, 1.0f, -N.y))));
+                float Kd = DotClamped(N, H);
+                float4 diffuse = Kd * float4(_LightColor0.rgb, 1.0f) * _Diffuse;
+
+                float base = 1 - dot(E, N);
+                float exponential = pow(base, 25.0);
+                float fresnel = exponential + F0 * (1.0 - exponential);
+
+                float Ks = pow(DotClamped(N, H), 100.0);
+                float4 spec = Ks * float4(_LightColor0.rgb, 1.0f) * _Specular * fresnel;
+
+                float4 ambient = _Ambient;
+
+             
+
+                float4 color = saturate(ambient + diffuse + spec);
+                return color;
+            }
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+
+                //o.worldPos = mul(unity_ObjectToWorld, v.vertex);
+                //o.normal = normalize(UnityObjectToWorldNormal(v.normal));
+                o.vertex = UnityObjectToClipPos(v.vertex + float3(0.0f, _HeightTex.SampleLevel(linear_repeat_sampler, v.uv * 0.99f, 0).r, 0.0f));
+                //o.uv = v.uv;
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = tex2D(_MainTex, i.uv);
+                // apply fog
+                UNITY_APPLY_FOG(i.fogCoord, col);
+                return float4(i.worldPos,1.0f);
+            }
+            ENDCG
+        }
+    }
+}
